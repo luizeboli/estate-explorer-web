493c045440a2921dc749a9fd964dffcb184eb15c
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2023-06-26 19:22:04 -0300

    chore(main): add meta description for the website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Navbar from '@/components/Navbar';
 
 export const metadata = {
 	title: 'Estate Explorer',
+	description: 'The best place to find your new home',
 };
 
 const inter = Inter({ subsets: ['latin'] });
