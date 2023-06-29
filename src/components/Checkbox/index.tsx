bfd66f3f971570884a8aa447bfdb602587adc313
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2023-06-29 18:31:37 -0300

    feat: create custom checkbox component

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,17 +1,78 @@
 'use client';
 
+import { css } from '@linaria/core';
+import { styled } from '@linaria/react';
 import { InputHTMLAttributes, useId } from 'react';
 
 type CheckboxProps = { label: string } & Omit<InputHTMLAttributes<HTMLInputElement>, 'type' | 'id'>;
+
+const Marker = styled.div`
+	position: relative;
+	width: 2rem;
+	height: 2rem;
+	border-radius: 0.4rem;
+	background-color: rgb(229 229 229);
+
+	&::after {
+		content: '';
+		position: absolute;
+		display: none;
+		left: 0.75rem;
+		top: 0.25rem;
+		width: 0.4rem;
+		height: 1rem;
+		border: solid white;
+		border-width: 0 2px 2px 0;
+		transform: rotate(45deg);
+	}
+`;
+
+const Input = styled.input`
+	position: absolute;
+	opacity: 0;
+	width: 0;
+	height: 0;
+
+	&:focus + ${Marker} {
+		outline: 1px solid rgb(168 85 247);
+		outline-offset: 2px;
+		outline-width: 2px;
+	}
+
+	&:checked + ${Marker} {
+		background-color: rgb(168 85 247);
+		&::after {
+			display: block;
+		}
+	}
+`;
+
+const labelClassName = css`
+	position: relative;
+	cursor: pointer;
+	display: flex;
+	gap: 0.8rem;
+
+	&:hover {
+		${Marker} {
+			background-color: rgb(212 212 212);
+		}
+
+		& ${Input}:checked + ${Marker} {
+			background-color: rgb(126 34 206);
+		}
+	}
+`;
 
 const Checkbox = (props: CheckboxProps) => {
 	const { label, ...inputProps } = props;
 	const id = useId();
 
 	return (
-		<label htmlFor={id}>
-			{/*  eslint-disable-next-line react/jsx-props-no-spreading */}
-			<input type="checkbox" id={id} {...inputProps} />
+		<label htmlFor={id} className={labelClassName}>
+			{/* eslint-disable-next-line react/jsx-props-no-spreading */}
+			<Input type="checkbox" id={id} {...inputProps} />
+			<Marker />
 			{label}
 		</label>
 	);
