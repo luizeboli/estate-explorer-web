3b5ab7cd097dc110b948808f81d476042fa34697
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2023-06-29 12:27:23 -0300

    refactor: get properties from api for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,68 +1,8 @@
 import FeaturedProperties from '@/components/FeaturedProperties';
 import HomeContent from '@/components/HomeContent';
 import HomeHero from '@/components/HomeHero';
-import { Property } from '@/types';
+import { getProperties } from '@/services/api';
 import { styled } from '@linaria/react';
-
-const properties: Property[] = [
-	{
-		id: 1,
-		cover: 'https://placehold.co/200.png',
-		title: 'Modern Family Home',
-		location: 'San Francisco',
-		price: 1234567890,
-		status: { slug: 'for-sale', name: 'For Sale' },
-		amenities: [
-			{ slug: 'park', name: 'Park' },
-			{ slug: 'pool', name: 'Pool' },
-			{ slug: 'gym', name: 'Gym' },
-		],
-	},
-	{
-		id: 2,
-		cover: 'https://placehold.co/200.png',
-		title: 'Tarpon Bay',
-		location: 'Orlando',
-		price: 3245,
-		status: { slug: 'for-rent', name: 'For Rent' },
-		amenities: [
-			{ slug: 'park', name: 'Park' },
-			{ slug: 'pool', name: 'Pool' },
-			{ slug: 'gym', name: 'Gym' },
-		],
-	},
-	{
-		id: 3,
-		cover: 'https://placehold.co/200.png',
-		title: 'Wonder Land',
-		location: 'St. Petersburg Beach Florida 33706',
-		price: 1000,
-		status: { slug: 'for-rent', name: 'For Rent' },
-		amenities: [{ slug: 'park', name: 'Park' }],
-	},
-	{
-		id: 4,
-		cover: 'https://placehold.co/200.png',
-		title: 'Cove Red',
-		location: 'Miami',
-		price: 300000,
-		status: { slug: 'for-sale', name: 'For Sale' },
-		amenities: [{ slug: 'gym', name: 'Gym' }],
-	},
-	{
-		id: 5,
-		cover: 'https://placehold.co/200.png',
-		title: 'Disneyland',
-		location: 'Orlando Florida 32830',
-		price: 3400,
-		status: { slug: 'for-sale', name: 'For Sale' },
-		amenities: [
-			{ slug: 'park', name: 'Park' },
-			{ slug: 'pool', name: 'Pool' },
-			{ slug: 'gym', name: 'Gym' },
-		],
-	},
-];
 
 const ContentWrapper = styled.div`
 	display: flex;
@@ -76,7 +16,9 @@ const ContentWrapper = styled.div`
 	}
 `;
 
-const HomePage = () => {
+const HomePage = async () => {
+	const properties = await getProperties({ params: { per_page: 10 } });
+
 	return (
 		<>
 			<HomeHero />
