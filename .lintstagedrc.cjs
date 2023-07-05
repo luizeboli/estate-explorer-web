e0bfcdca39a40454a96c37eed85d89e091a7629f
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2023-07-05 14:17:22 -0300

    chore: rename lintstaged config file so its linted

--- .lintstagedrc.cjs
+++
@@ -1,9 +0,0 @@
-const typeCheck = () => `tsc --project ./tsconfig.json --noEmit`
-
-module.exports = {
-  '*.{ts,tsx}': [
-    'eslint --report-unused-disable-directives --max-warnings 0 --cache --fix',
-    typeCheck,
-    "jest --bail --findRelatedTests"
-  ],
-}
