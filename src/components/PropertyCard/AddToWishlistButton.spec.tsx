8e9682504a35b37759cf7653909dca672b13d1f0
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2023-07-05 15:15:09 -0300

    test: add coverage for wishlist button

---
+++ src/components/PropertyCard/AddToWishlistButton.spec.tsx
@@ -0,0 +1,45 @@
+import { render, screen } from '@/tests/utils';
+import { Property } from '@/types';
+import userEvent from '@testing-library/user-event';
+import colors from '@/styles/colors';
+import * as PropertyCard from '.';
+import AddToWishlistButton from './AddToWishlistButton';
+
+describe('<AddToWishlistButton />', () => {
+	it('should add a property to the wishlist', async () => {
+		// Arrange
+		const user = userEvent.setup();
+
+		render(
+			<PropertyCard.Root property={{ id: 123 } as Property}>
+				<AddToWishlistButton />
+			</PropertyCard.Root>,
+		);
+
+		// Act
+		await user.click(screen.getByLabelText('Add to wishlist'));
+
+		// Assert
+		expect(screen.getByLabelText('Remove from wishlist')).toBeInTheDocument();
+		expect(screen.getByTestId('wishlist-icon')).toHaveAttribute('fill', colors.purple[500]);
+	});
+
+	it('should remove a property from the wishlist', async () => {
+		// Arrange
+		const user = userEvent.setup();
+
+		render(
+			<PropertyCard.Root property={{ id: 123 } as Property}>
+				<AddToWishlistButton />
+			</PropertyCard.Root>,
+		);
+
+		// Act
+		await user.click(screen.getByLabelText('Add to wishlist'));
+		await user.click(screen.getByLabelText('Remove from wishlist'));
+
+		// Assert
+		expect(screen.getByLabelText('Add to wishlist')).toBeInTheDocument();
+		expect(screen.getByTestId('wishlist-icon')).toHaveAttribute('fill', 'none');
+	});
+});
--- src/components/PropertyCard/AddToWishlistButton.tsx
+++ src/components/PropertyCard/AddToWishlistButton.tsx
@@ -1,20 +1,8 @@
 import { useWishlistContext } from '@/components/WishlistProvider/context';
-import { styled } from '@linaria/react';
 import { Heart } from 'lucide-react';
-import IconButton from '@/components/IconButton';
 import colors from '@/styles/colors';
+import IconButton from '@/components/IconButton';
 import { usePropertyContext } from './context';
-
-type StyledIconButtonProps = {
-	isWishlisted: boolean;
-};
-
-const StyledIconButton = styled(IconButton)<StyledIconButtonProps>`
-	& svg {
-		fill: ${({ isWishlisted }) => (isWishlisted ? colors.purple[500] : 'none')};
-		color: ${colors.purple[500]};
-	}
-`;
 
 const AddToWishlistButton = () => {
 	const { addToWishlist, removeFromWishlist, isInWishlist } = useWishlistContext();
@@ -30,13 +18,17 @@ const AddToWishlistButton = () => {
 	};
 
 	return (
-		<StyledIconButton
+		<IconButton
 			onClick={handleWishlistClick}
 			aria-label={isWishlisted ? 'Remove from wishlist' : 'Add to wishlist'}
-			isWishlisted={isWishlisted}
 		>
-			<Heart size={20} />
-		</StyledIconButton>
+			<Heart
+				data-testid="wishlist-icon"
+				size={20}
+				color={colors.purple[500]}
+				fill={isWishlisted ? colors.purple[500] : 'none'}
+			/>
+		</IconButton>
 	);
 };
 export default AddToWishlistButton;
