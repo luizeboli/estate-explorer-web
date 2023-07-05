d11e8aec965614fefbc164b5726b69e620377ac1
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2023-07-05 15:46:43 -0300

    test: add coverage for amenity icons component

---
+++ src/components/AmenityIcon/index.spec.tsx
@@ -0,0 +1,27 @@
+import { render } from '@/tests/utils';
+import { AmenityTermSlug } from '@/types/taxonomy';
+import AmenityIcon from '.';
+
+describe('<AmenityIcon />', () => {
+	it.each<{ slug: AmenityTermSlug; icon: string }>([
+		{
+			slug: 'gym',
+			icon: 'dumbbell',
+		},
+		{
+			slug: 'parking',
+			icon: 'parking-square',
+		},
+		{
+			slug: 'pool',
+			icon: 'waves',
+		},
+	])('should render the "$icon" icon for "$slug" amenity', ({ icon, slug }) => {
+		// Arrange
+		const { container } = render(<AmenityIcon slug={slug} />);
+
+		// Assert
+		const iconElement = container.getElementsByClassName(`lucide-${icon}`)[0];
+		expect(iconElement).toBeInTheDocument();
+	});
+});
