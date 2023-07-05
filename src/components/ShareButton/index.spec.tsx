b0a5af035494bb1e9483392db8f82bc5f37be92b
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2023-07-05 13:55:39 -0300

    test: add coverage for share button

---
+++ src/components/ShareButton/index.spec.tsx
@@ -0,0 +1,37 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import ShareButton from '.';
+
+Object.defineProperty(window, 'location', {
+	value: {
+		href: 'https://www.example.com',
+	},
+	writable: true,
+});
+
+describe('<ShareButton />', () => {
+	it('should copy the current URL to the clipboard', async () => {
+		// Arrange
+		jest.useFakeTimers();
+
+		const writeText = jest.fn();
+		Object.assign(navigator, {
+			clipboard: {
+				writeText,
+			},
+		});
+
+		render(<ShareButton />);
+
+		// Act
+		userEvent.click(screen.getByRole('button', { name: /share/i }));
+
+		// Assert
+		await waitFor(() => expect(writeText).toHaveBeenCalledWith('https://www.example.com'));
+		expect(screen.getByRole('button', { name: /link copied/i })).toBeInTheDocument();
+
+		jest.runAllTimers();
+
+		expect(await screen.findByRole('button', { name: /share/i })).toBeInTheDocument();
+	});
+});
