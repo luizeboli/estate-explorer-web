68501d3fab26c0191b262e7d61b6b6d4b79ea524
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2023-09-19 18:01:23 -0300

    Revert "Fix static generation after mocking properties"
    This reverts commit ee90c845996b91a41fd51dba4d67000b3bec2c8c.

--- src/app/property/[slug]/page.tsx
+++ src/app/property/[slug]/page.tsx
@@ -11,9 +11,6 @@ import breakpoints, { screenMinWidth } from '@/styles/breakpoints';
 import ShareButton from '@/components/ShareButton';
 import colors from '@/styles/colors';
 import type { Metadata } from 'next';
-import { wordPressProperties } from '@/mocks/properties';
-import { normalizeWordpressProperties } from '@/utils/wordpress';
-import type { WordpressPropertyPostType } from '@/types/wordpress';
 
 const Wrapper = styled.div`
 	padding: 5.4rem 5vw 0;
@@ -222,9 +219,7 @@ const PropertyPage = async ({ params }: PropertyPageProps) => {
 export default PropertyPage;
 
 export const generateStaticParams = async () => {
-	const properties = normalizeWordpressProperties(
-		wordPressProperties as WordpressPropertyPostType[],
-	);
+	const properties = await getProperties({ params: { per_page: 50 } });
 
 	return properties.map(({ slug }) => ({ slug }));
 };
